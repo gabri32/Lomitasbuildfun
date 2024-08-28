0c5c60b6a7abea17930b365136013ba044b98d4b
gabri32 <88991735+gabri32@users.noreply.github.com>
2024-08-28 01:40:03 -0500

    full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Directive,Component, OnInit,ElementRef, Renderer2,HostListener} from '@
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
